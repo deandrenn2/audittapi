c29b7df8a7666abcf370231d8340a88e16f6ef66
Deimer Andres Nuñez Novoa <deandrenn@hotmail.com>
2025-04-18 18:09:57 -0500

    Add logic valutation

---
+++ Auditt.Application/Features/Assessments/Queries/GetAssessment.cs
@@ -0,0 +1,63 @@
+using Carter;
+using Carter.ModelBinding;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Auditt.Application.Domain.Entities;
+using Auditt.Application.Infrastructure.Sqlite;
+using Auditt.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auditt.Application.Features.Assessments;
+
+public class GetAssessment : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/assessment/{id}", async (HttpRequest req, IMediator mediator, int id) =>
+        {
+            return await mediator.Send(new GetAssessmentQuery(id));
+        })
+        .WithName(nameof(GetAssessment))
+        .WithTags(nameof(Assessment))
+        .ProducesValidationProblem()
+        .Produces<GetAssessmentResponse>(StatusCodes.Status200OK);
+    }
+    public record GetAssessmentQuery(int Id) : IRequest<IResult>;
+    public record GetAssessmentResponse(int Id, int IdDataCut, int IdFunctionary, int IdPatient, string YearOld, DateTime Date, string Eps, int IdUserCreated, int IdUserUpdate, DateTime UpdateDate, DateTime CreateDate, List<ValuationModel> Valuations);
+    public record ValuationModel(int Id, int Order, string Text, int IdAssessment, int IdEquivalence, int? IdQuestion);
+    public class GetAssessmentHandler(AppDbContext context, IValidator<GetAssessmentQuery> validator) : IRequestHandler<GetAssessmentQuery, IResult>
+    {
+        public async Task<IResult> Handle(GetAssessmentQuery request, CancellationToken cancellationToken)
+        {
+            var result = validator.Validate(request);
+            if (!result.IsValid)
+            {
+                return Results.Ok(Result<IResult>.Failure(Results.ValidationProblem(result.GetValidationProblems()), new Error("Login.ErrorValidation", "Se presentaron errores de validación")));
+            }
+            var assessment = await context.Assessments.Include(x => x.Valuations).Where(x => x.Id == request.Id).FirstOrDefaultAsync(cancellationToken);
+
+
+
+            if (assessment == null)
+            {
+                return Results.NotFound(Result<IResult>.Failure(Results.NotFound(), new Error("Assessment.NotFound", "No se encontró la evaluación")));
+            }
+
+            var valuations = assessment.Valuations.Select(x => new ValuationModel(x.Id, x.Order, x.Text, x.IdAssessment, x.IdEquivalence, x.IdQuestion)).ToList();
+
+
+            var response = new GetAssessmentResponse(assessment.Id, assessment.IdDataCut, assessment.IdFunctionary, assessment.IdPatient, assessment.YearOld, assessment.Date, assessment.Eps, assessment.IdUserCreated, assessment.IdUserUpdate, assessment.UpdateDate, assessment.CreateDate, valuations);
+            return Results.Ok(Result<GetAssessmentResponse>.Success(response, "Se obtuvo la evaluación correctamente"));
+        }
+    }
+    public class GetAssessmentValidator : AbstractValidator<GetAssessmentQuery>
+    {
+        public GetAssessmentValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("El id no puede estar vacío");
+        }
+    }
+}
---
+++ Auditt.Application/Features/Assessments/Queries/GetAssessments.cs
@@ -0,0 +1,51 @@
+using Carter;
+using Carter.ModelBinding;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Auditt.Application.Domain.Entities;
+using Auditt.Application.Infrastructure.Sqlite;
+using Auditt.Domain.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auditt.Application.Features.Assessments;
+
+public class GetAssessments : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/assessment", async (HttpRequest req, IMediator mediator) =>
+        {
+            return await mediator.Send(new GetAssessmentsQuery());
+        })
+        .WithName(nameof(GetAssessments))
+        .WithTags(nameof(Assessment))
+        .ProducesValidationProblem()
+        .Produces<List<GetAssessmentsResponse>>(StatusCodes.Status200OK);
+    }
+    public record GetAssessmentsQuery() : IRequest<IResult>;
+    public record GetAssessmentsResponse(int Id, string IdentificationPatient, string FunctionaryName, DateTime Date);
+    public class GetAssessmentsHandler(AppDbContext context, IValidator<GetAssessmentsQuery> validator) : IRequestHandler<GetAssessmentsQuery, IResult>
+    {
+        public async Task<IResult> Handle(GetAssessmentsQuery request, CancellationToken cancellationToken)
+        {
+            var result = validator.Validate(request);
+            if (!result.IsValid)
+            {
+                return Results.Ok(Result<IResult>.Failure(Results.ValidationProblem(result.GetValidationProblems()), new Error("Login.ErrorValidation", "Se presentaron errores de validación")));
+            }
+            var assessments = await context.Assessments.Include(x => x.Patient).Include(x => x.Functionary).Include(x => x.Valuations).ToListAsync(cancellationToken);
+            var response = assessments.Select(x => new GetAssessmentsResponse(x.Id, x.Patient.Identification, $"{x.Functionary.LastName} {x.Functionary.FirstName}", x.Date)).ToList();
+            return Results.Ok(Result<List<GetAssessmentsResponse>>.Success(response, "Se obtuvo la evaluación correctamente"));
+        }
+    }
+    public class GetAssessmentsValidator : AbstractValidator<GetAssessmentsQuery>
+    {
+        public GetAssessmentsValidator()
+        {
+            // No validation rules needed for this query
+        }
+    }
+}
