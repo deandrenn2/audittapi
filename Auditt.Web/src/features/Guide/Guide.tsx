d4072e8603b81a391763ffb4afcd584ade4724ff
James Bravo <jameszuniga348@gmail.com>
2025-04-23 21:01:07 -0500

    The add question function was created

--- Auditt.Web/src/features/Guide/Guide.tsx
+++ Auditt.Web/src/features/Guide/Guide.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom"; 
 import ButtonDetail from "../../shared/components/Buttons/ButtonDetail";
 import OffCanvas from "../../shared/components/OffCanvas/Index";
 import { Direction } from "../../shared/components/OffCanvas/Models";
@@ -10,36 +11,36 @@ import { Bar } from "../../shared/components/Progress/Bar";
 import { GuideModel } from "./GuideModel";
 import { GuideUpdate } from "./GuideUpdate";
 import { ButtonPlay } from "../../shared/components/Buttons/ButtonPlay";
+import { useQuestions } from "./Questions/useQuestions";
 
 export const Guide = () => {
     const [visible, setVisible] = useState(false);
     const [visibleUpdate, setUpdateVisible] = useState(false);
     const { guides, queryGuide, deleteGuide } = useGuide();
     const [guide, setGuide] = useState<GuideModel>();
-    const [idGuide, setIdGuide] = useState(0);
-
+    const { questions } = useQuestions();
 
     const handleGuideDetail = (guideSelected: GuideModel) => {
         setGuide(guideSelected);
         setUpdateVisible(true);
-        setIdGuide(idGuide)
+
     };
 
     function handleDelete(e: React.MouseEvent<HTMLButtonElement>, id: number): void {
-                e.preventDefault();
-                Swal.fire({
-                    title: '¿Estás seguro de eliminar esta Equivalencia?',
-                    text: 'Esta acción no se puede deshacer',
-                    icon: 'warning',
-                    showCancelButton: true,
-                    confirmButtonColor: '#3085d6',
-                    confirmButtonText: 'Confirmar',
-                    cancelButtonText: 'Cancelar',
-                    preConfirm: async () => {
-                        await deleteGuide.mutateAsync(id);
-                    }
-                })
+        e.preventDefault();
+        Swal.fire({
+            title: '¿Estás seguro de eliminar esta Equivalencia?',
+            text: 'Esta acción no se puede deshacer',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Confirmar',
+            cancelButtonText: 'Cancelar',
+            preConfirm: async () => {
+                await deleteGuide.mutateAsync(id);
             }
+        });
+    }
 
     if (queryGuide.isLoading) return <Bar />;
 
@@ -47,17 +48,15 @@ export const Guide = () => {
         <div className="flex p-8">
             <div>
                 <h2 className="text-2xl font-semibold mb-6 mr-2">Instrumentos o GUIAS</h2>
-
                 <button onClick={() => setVisible(true)} className="bg-indigo-500 hover:bg-indigo-900 text-white px-6 py-2 rounded-lg font-semibold mb-2">
                     Crear Instrumento
                 </button>
-
                 <div>
                     <div className="grid grid-cols-4">
-                        <div className="grid grid-cols-3 gap-3 font-semibold bg-gray-300  text-gray-800 px-2 py-1 ">Nombre</div>
-                        <div className="grid grid-cols-3 gap-3 font-semibold bg-gray-300  text-gray-800 px-2 py-1 ">Descripción</div>
-                        <div className="grid grid-cols-3 gap-3 font-semibold bg-gray-300  text-gray-800 px-2 py-1 ">Preguntas</div>
-                        <div className="grid grid-cols-3 gap-3 font-semibold bg-gray-300  text-gray-800 px-2 py-1 text-center">Opciones</div>
+                        <div className="grid grid-cols-3 gap-3 font-semibold bg-gray-300 text-gray-800 px-2 py-1">Nombre</div>
+                        <div className="grid grid-cols-3 gap-3 font-semibold bg-gray-300 text-gray-800 px-2 py-1">Descripción</div>
+                        <div className="grid grid-cols-3 gap-3 font-semibold bg-gray-300 text-gray-800 px-2 py-1">Preguntas</div>
+                        <div className="grid grid-cols-3 gap-3 font-semibold bg-gray-300 text-gray-800 px-2 py-1 text-center">Opciones</div>
                     </div>
 
                     <div className="bg-white px-2 py-2 border border-gray-200">
@@ -65,24 +64,25 @@ export const Guide = () => {
                             <div className="grid grid-cols-4" key={guide.id}>
                                 <div className="grid grid-cols-3 gap-3 text-sm bg-white px-2 py-2 border border-gray-300 mr-2 mb-2">{guide.name}</div>
                                 <div className="grid grid-cols-3 gap-3 text-sm bg-white px-2 py-2 border border-gray-300 mr-2 mb-2">{guide.description}</div>
-                                <div className="grid grid-cols-3 gap-3 text-sm bg-white px-2 py-2 border border-gray-300 mr-2 mb-2">80</div>
+                                <div className="grid grid-cols-3 gap-3 text-sm bg-white px-2 py-2 border border-gray-300 mr-2 mb-2">{questions?.filter(q => q.idGuide === guide.id).length ?? 0}</div>
                                 <div className="flex justify-center">
                                     <div onClick={() => handleGuideDetail(guide)}>
                                         <ButtonPlay />
                                     </div>
+                                    <Link to={`/questions/${guide.id}`}>
                                         <ButtonDetail url={"Questions"} />
-                                
+                                    </Link>
                                     <ButtonDelete id={guide.id ?? 0} onDelete={handleDelete} />
                                 </div>
                             </div>
                         ))}
                     </div>
                 </div>
-                <OffCanvas titlePrincipal='Crear Instrumentos' visible={visible} xClose={() => setVisible(false)} position={Direction.Right}>
+                <OffCanvas titlePrincipal="Crear Instrumentos" visible={visible} xClose={() => setVisible(false)} position={Direction.Right}>
                     <GuidesCreate />
                 </OffCanvas>
                 {guide && (
-                    <OffCanvas titlePrincipal='Detalle Instrumentos' visible={visibleUpdate} xClose={() => setUpdateVisible(false)} position={Direction.Right}>
+                    <OffCanvas titlePrincipal="Detalle Instrumentos" visible={visibleUpdate} xClose={() => setUpdateVisible(false)} position={Direction.Right}>
                         <GuideUpdate data={guide} />
                     </OffCanvas>
                 )}
