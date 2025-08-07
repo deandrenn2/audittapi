653cfbfd70b29c6e9da6baff7e5384e8c8b5320e
Deimer Andres Nuñez Novoa <deandrenn@hotmail.com>
2025-08-06 22:22:23 -0500

    Enhance GetAssessment response structure with additional fields and include related entities

--- Auditt.Application/Features/Assessments/Queries/GetAssessment.cs
+++ Auditt.Application/Features/Assessments/Queries/GetAssessment.cs
@@ -26,7 +26,23 @@ public class GetAssessment : ICarterModule
         .Produces<GetAssessmentResponse>(StatusCodes.Status200OK);
     }
     public record GetAssessmentQuery(int Id) : IRequest<IResult>;
-    public record GetAssessmentResponse(int Id, int IdDataCut, int IdFunctionary, int IdPatient, string YearOld, DateTime Date, string Eps, int IdUserCreated, int IdUserUpdate, DateTime UpdateDate, DateTime CreateDate, List<ValuationModel> Valuations);
+    public record GetAssessmentResponse(
+        int Id,
+        int IdDataCut,
+        int IdFunctionary,
+        string FunctionaryName,
+        int IdPatient,
+        string Identification,
+        string YearOld,
+        DateTime Date,
+        string Eps,
+        int IdUserCreated,
+        int IdUserUpdate,
+        DateTime UpdateDate,
+        DateTime CreateDate,
+        int IdGuide,
+        string GuideName,
+        List<ValuationModel> Valuations);
     public record ValuationModel(int Id, int Order, string Text, int IdAssessment, int IdEquivalence, int? IdQuestion);
     public class GetAssessmentHandler(AppDbContext context, IValidator<GetAssessmentQuery> validator) : IRequestHandler<GetAssessmentQuery, IResult>
     {
@@ -37,7 +53,12 @@ public class GetAssessment : ICarterModule
             {
                 return Results.Ok(Result<IResult>.Failure(Results.ValidationProblem(result.GetValidationProblems()), new Error("Login.ErrorValidation", "Se presentaron errores de validación")));
             }
-            var assessment = await context.Assessments.Include(x => x.Valuations).Where(x => x.Id == request.Id).FirstOrDefaultAsync(cancellationToken);
+            var assessment = await context.Assessments
+                        .Include(x => x.Valuations)
+                        .Include(x => x.Functionary)
+                        .Include(x => x.Patient)
+                        .Include(x => x.Guide)
+                        .Where(x => x.Id == request.Id).FirstOrDefaultAsync(cancellationToken);
 
 
 
@@ -49,7 +70,24 @@ public class GetAssessment : ICarterModule
             var valuations = assessment.Valuations.Select(x => new ValuationModel(x.Id, x.Order, x.Text, x.AssessmentId, x.EquivalenceId, x.IdQuestion)).ToList();
 
 
-            var response = new GetAssessmentResponse(assessment.Id, assessment.DataCutId, assessment.FunctionaryId, assessment.PatientId, assessment.YearOld, assessment.Date, assessment.Eps, assessment.IdUserCreated, assessment.IdUserUpdate, assessment.UpdateDate, assessment.CreateDate, valuations);
+            var response = new GetAssessmentResponse(
+                assessment.Id,
+                assessment.DataCutId,
+                assessment.FunctionaryId,
+                assessment.Functionary?.FirstName + " " + (assessment.Functionary?.LastName ?? string.Empty),
+                assessment.PatientId,
+                assessment.Patient?.Identification ?? string.Empty,
+                assessment.YearOld,
+                assessment.Date,
+                assessment.Eps,
+                assessment.IdUserCreated,
+                assessment.IdUserUpdate,
+                assessment.UpdateDate,
+                assessment.CreateDate,
+                assessment.GuideId,
+                assessment.Guide?.Name ?? string.Empty,
+                valuations
+                );
             return Results.Ok(Result<GetAssessmentResponse>.Success(response, "Se obtuvo la evaluación correctamente"));
         }
     }
